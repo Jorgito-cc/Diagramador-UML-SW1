1aa32db007980da6900fbaac9bc421ad604f51e4
jorgito-cc <jorgitochoque007@gmail.com>
2025-10-01 11:32:43 -0400

    feat ultimos cabios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  geminiApiKey: 'AIzaSyCUtAZ19ZhTUcsI9Sy5cxp-bm_DRbjx9yk'  // Aquí tu nueva clave de DeepSeek
+  geminiApiKey: 'AIzaSyCMtIgM2jeMAr1CJJIbhBCOA_Yw_BmLBRQ'  // Aquí tu nueva clave de DeepSeek
+  // otra api AIzaSyCUtAZ19ZhTUcsI9Sy5cxp-bm_DRbjx9yk
+  // api de prueba AIzaSyCMtIgM2jeMAr1CJJIbhBCOA_Yw_BmLBRQ
 };
 
 
